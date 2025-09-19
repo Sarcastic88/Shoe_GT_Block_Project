566f531b96afe247b98e2ca2c632d2b6444b1b08
Sarcastic88 <kaiorenstein@gmail.com>
2025-09-18 21:20:09 -0400

    update color

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xaaaaaa);
+renderer.setClearColor(0x0000000);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 renderer.shadowMap.enabled = true;
